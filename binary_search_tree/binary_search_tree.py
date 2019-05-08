b8e3e72c5ba0a8f4517d0b5393b02ca46b362c5f
KROHNICK <business.kanekperez@gmail.com>
2019-05-08 11:15:40 -0700

    finished binary search

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,45 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        if value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        if self.value == target:
+            return True
 
-  def get_max(self):
-    pass
+        if target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
 
-  def for_each(self, cb):
-    pass
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
